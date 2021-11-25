5e55bdc7f341dd480c9fd3980e92d1edadfea6e8
Oleg <oleg.zabello@p2h.com>
2021-11-25 18:14:14 +0200

    fixed

---
+++ markup/mail1.php
@@ -0,0 +1,34 @@
+<?php
+	//****************************************
+	//edit here
+	$senderName = 'BlackFriday';
+	$senderEmail = '';
+	$targetEmail = 'zab.oleg.sv@gmail.com';
+	$messageSubject = 'Message from BlackFriday';
+	$redirectToReferer = false;
+	$redirectURL = 'thankyou.html';
+	//****************************************
+
+	// mail content
+	$email = $_POST['email'];
+
+	// prepare message text
+	$messageText =	'E-mail: '.$email."\n";
+
+	// send email
+	$senderName = "=?UTF-8?B?" . base64_encode($senderName) . "?=";
+	$messageSubject = "=?UTF-8?B?" . base64_encode($messageSubject) . "?=";
+	$messageHeaders = "From: " . $senderName . " <" . $senderEmail . ">\r\n"
+				. "MIME-Version: 1.0" . "\r\n"
+				. "Content-type: text/plain; charset=UTF-8" . "\r\n";
+
+	if (preg_match('/^[_.0-9a-z-]+@([0-9a-z][0-9a-z-]+.)+[a-z]{2,4}$/',$targetEmail,$matches))
+	mail($targetEmail, $messageSubject, $messageText, $messageHeaders);
+
+	// redirect
+	if($redirectToReferer) {
+		header("Location: ".@$_SERVER['HTTP_REFERER'].'#sent');
+	} else {
+		header("Location: ".$redirectURL);
+	}
+?>
